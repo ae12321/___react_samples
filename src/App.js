cf106f96bbe76c0740dc93c515f77f9d0ba793f7
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-29 22:45:47 +0900

    get detail data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
 
   const [loading, setLoading] = useState(true);
 
+  const [pokemonData, setPokemonData] = useState([]);
+
   useEffect(() => {
     const fetchPokemonData = async () => {
       // all pokemon data
       const res = await getAllPokemon(initialURL);
-      console.log(res);
+
+      // detail data
+      // res = {count: number; next: string(url); previous: ?; results: [{ name: string; url: string(url) }]}
+      loadPokemon(res.results);
+
+      // console.log(res);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let allDetail = await Promise.all(
+      data.map((pokemon) => {
+        console.log(pokemon);
+        let detail = getPokemon(pokemon.url);
+        return detail;
+      })
+    );
+    setPokemonData(allDetail);
+  };
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
