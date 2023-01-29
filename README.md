67a32b0a3325b2cbbd0db59e998eaa2115b26e9a
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 00:44:57 +0900

    add font by fontawesome

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ created project settning > myapp
 easy routing
 
 - `npm install react-router-dom`
+
+try fontawesome
+
+https://fontawesome.com/docs/web/use-with/react/
+
+- `npm i --save @fortawesome/fontawesome-svg-core`
+- `npm i --save @fortawesome/free-solid-svg-icons`
+- `npm i --save @fortawesome/free-regular-svg-icons`
+- `npm i --save @fortawesome/react-fontawesome@latest`
+
+https://fontawesome.com/docs/web/use-with/react/add-icons
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// https://fontawesome.com/icons -> fa-house -> faHouse
+import {
+  faHouse,
+  faCalendarPlus,
+  faRightToBracket,
+} from '@fortawesome/free-solid-svg-icons';
+
+// const element = <FontAwesomeIcon icon={faEnvelope} />;
+
 const Navbar = () => {
   return (
     <nav>
-      <Link to={'/'}>home</Link>
-      <Link to={'/create'}>create</Link>
-      <Link to={'/login'}>login</Link>
+      <Link to={'/'}>
+        <FontAwesomeIcon icon={faHouse} />
+        home
+      </Link>
+      <Link to={'/create'}>
+        <FontAwesomeIcon icon={faCalendarPlus} />
+        create
+      </Link>
+      <Link to={'/login'}>
+        <FontAwesomeIcon icon={faRightToBracket} /> login
+      </Link>
     </nav>
   );
 };
