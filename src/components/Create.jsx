a61aa3616b360c52a3216d4043e43b25914d123a
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 01:31:13 +0900

    modify post page

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Create.css';
 
 const Create = () => {
-  return <div>Create</div>;
+  return (
+    <div className="createPost">
+      <div className="container">
+        <h1>post entry</h1>
+        <div className="inputPost">
+          <div>title</div>
+          <input type="text" placeholder="input title" />
+        </div>
+      </div>
+      <div className="inputPost">
+        <div>body</div>
+        <textarea placeholder="input body"></textarea>
+      </div>
+      <button className="postbutton">send</button>
+    </div>
+  );
 };
 
 export default Create;
