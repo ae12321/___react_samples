93131b7616c70dfa8575f1a3efde3ac1da438c05
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 01:46:49 +0900

    hold title/post data using usestate

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Create.css';
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
+  const createPost = () => {
+    console.log({ title, postText });
+  };
   return (
     <div className="createPost">
       {/* some parts move all at once*/}
@@ -9,13 +15,23 @@ const Create = () => {
         <h1>post entry</h1>
         <div className="inputPost">
           <div>title</div>
-          <input type="text" placeholder="input title" />
+          <input
+            type="text"
+            placeholder="input title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>body</div>
-          <textarea placeholder="input body"></textarea>
+
+          <textarea
+            placeholder="input body"
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postbutton">send</button>
+        <button className="postbutton" onClick={createPost}>
+          send
+        </button>
       </div>
     </div>
   );
