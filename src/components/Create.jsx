5e4317269d58147dc02813040948db6b5124294d
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 01:42:30 +0900

    add cssto post page

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -4,18 +4,19 @@ import './Create.css';
 const Create = () => {
   return (
     <div className="createPost">
+      {/* some parts move all at once*/}
       <div className="container">
         <h1>post entry</h1>
         <div className="inputPost">
           <div>title</div>
           <input type="text" placeholder="input title" />
         </div>
+        <div className="inputPost">
+          <div>body</div>
+          <textarea placeholder="input body"></textarea>
+        </div>
+        <button className="postbutton">send</button>
       </div>
-      <div className="inputPost">
-        <div>body</div>
-        <textarea placeholder="input body"></textarea>
-      </div>
-      <button className="postbutton">send</button>
     </div>
   );
 };
