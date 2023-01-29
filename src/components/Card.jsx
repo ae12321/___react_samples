883ed8ef494adff548661b9fce81c5ce838db6b9
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-29 23:00:23 +0900

    detail data creaetd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImage">
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div className="typeName">
+          type: {pokemon.types.map((type) => type.type.name).join(',')}
+        </div>
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">height: {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">weight: {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            abilities: {pokemon.abilities.map((a) => a.ability.name).join(',')}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
