a24ee85f76126182403f09c0ae120dd29a99789f
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 22:55:30 +0900

    sidebar create

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import './SideBar.css';
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h1>Note</h1>
+        <button>Add</button>
+      </div>
+      <div className="sidebar-notes">
+        <div className="sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>Title</strong>
+            <button>Delete</button>
+          </div>
+          <p>This is Note...</p>
+          <small>Last Edit: xx:xx</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
