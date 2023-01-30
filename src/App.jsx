eb5bd48257d15624d8e0dab0ba37c6756cc6e368
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 22:52:30 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Main from './components/Main';
+import SideBar from './components/SideBar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SideBar />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
