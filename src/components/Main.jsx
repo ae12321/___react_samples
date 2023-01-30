9dcda48040764f6c4e4306e6b9f491a037582c21
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 23:44:41 +0900

    add css to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './Main.css';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="main">
+      <div className="note-edit">
+        <input type="note-edit-text" />
+        <textarea placeholder="input note..."></textarea>
+      </div>
+      <div className="note-preview">
+        <h2 className="note-preview-title">title</h2>
+        <div className="note-preview-markdown">convert markdown</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
