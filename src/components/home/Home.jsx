2f5749896a6b0f99088401a9e635e54e48eb4afe
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-30 22:25:52 +0900

    get data from firestore

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from '../../firebase';
 
 import './Home.css';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      const customized = data.docs.map((doc) => {
+        // console.log(doc.id, ' => ', doc.data().author); // {username:string, id:string}
+        // console.log(doc.id, ' => ', doc.data()); //
+        return {
+          ...doc.data(),
+          id: doc.id,
+        };
+      });
+      // console.log(customized);
+      setPostList(customized);
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="home_container">
-      <div className="home_contents">
-        <div className="home_header">
-          <h1>title</h1>
-        </div>
+      {postList.map((post, index) => {
+        return (
+          <div key={index} className="home_contents">
+            <div className="home_header">
+              <h1>{post.title}</h1>
+            </div>
 
-        <div className="home_post">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt commodi
-          repellendus corrupti temporibus voluptates quos laudantium nihil
-          exercitationem dolores? Autem pariatur repellat molestias totam
-          quaerat! Tenetur modi
-          consequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasdsequaaaaasdfasdfasdfasduntur
-          eius temporibus?
-        </div>
-        <div className="home_parts">
-          <h4>@user</h4>
-          <button>Delete</button>
-        </div>
-      </div>
+            <div className="home_post">{post.postText}</div>
+            <div className="home_parts">
+              <h4>{post.author.username}</h4>
+              <button>Delete</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
